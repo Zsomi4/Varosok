c8fbc9db3472821ed3d61f34778e7771b71b5a45
Zsomi4 <zsomidev@gmail.com>
2022-03-16 11:20:24 +0100

    Tömb lezárása az 50. elemtől.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class App {
+    static final int MAX_VAROS = 5;
     public static void main(String[] args) throws Exception {
         System.out.println("Gubis Zsombor Dániel, 2022.03.16");
         String[] tomb = beker();
@@ -8,23 +9,31 @@ public class App {
     }
     public static String[] beker() {
         Scanner scan = new Scanner(System.in);
-        final int MAX_VAROS = 50;
         String[] varosok = new String[MAX_VAROS];
         int i = 0;
+
+        System.out.println("Maximum ennyi város tárolható: " + MAX_VAROS);
         String varos;
         do {
-            System.out.print("Város: ");
-            varos = scan.nextLine();
-            if(!varos.equalsIgnoreCase("vege")) {
+            if(i < MAX_VAROS) {
+                System.out.print("Város: ");
+                varos = scan.nextLine();
+                if(!varos.equalsIgnoreCase("vege")) {
                 varosok[i] = varos;
                 i++;
+                }
+            
+            } else {
+                System.out.println("Nincs több helye.");
+                System.out.println("Folytatáshoz Enter.");
+                varos = "vege";
             }
         }while(!varos.equals("vege"));
             return varosok;
     }
 
     public static void kiir(String[] tomb) {
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.print(tomb[i] + " ");
         }            
     }
